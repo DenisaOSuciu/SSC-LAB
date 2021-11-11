43b7e671119ffab52c371d200b377b007af81ff2
DenisaOSuciu <suciu.denisa33@gmail.com>
2021-11-11 13:47:36 +0200

    encrypt/decrypt messages using DES, 3DES, Rijndael

---
+++ tema2/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace tema2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Key and IV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(224, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 188);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(288, 215);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(224, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(288, 285);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(224, 20);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(288, 311);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(224, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 31);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Decrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 370);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Get Encrypt Time";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 415);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(119, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Get Decrypt Time";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DES",
+            "3DES",
+            "Rijndael"});
+            this.comboBox1.Location = new System.Drawing.Point(10, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(285, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "IV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Plain Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(252, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "ASCII";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "HEX";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(285, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Key";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(252, 288);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "ASCII";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(255, 314);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "HEX";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(267, 375);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(252, 415);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ tema2/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+
+
+namespace tema2 
+{
+
+    public partial class Form1 : Form
+    {
+       ConversionHandler myConverter = new ConversionHandler();
+        SymmetricAlgorithm mySymmetricAlg;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Generate(string cipher)
+        {
+
+            switch (cipher)
+            {
+                case "DES":
+                    mySymmetricAlg = DES.Create();
+                    break;
+                case "3DES":
+                    mySymmetricAlg = TripleDES.Create();
+                    break;
+                case "Rijndael":
+                    mySymmetricAlg = Rijndael.Create();
+                    break;
+            }
+            mySymmetricAlg.GenerateIV(); mySymmetricAlg.GenerateKey();
+        }
+        public byte[] Encrypt(byte[] mess, byte[] key, byte[] iv)
+        {
+            mySymmetricAlg.Key = key;
+            mySymmetricAlg.IV = iv;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, mySymmetricAlg.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(mess, 0, mess.Length); cs.Close();
+            return ms.ToArray();
+        }
+
+        public byte[] Decrypt(byte[] mess, byte[] key, byte[] iv)
+        {
+            byte[] plaintext = new byte[mess.Length];
+            mySymmetricAlg.Key = key; 
+            mySymmetricAlg.IV = iv;
+            MemoryStream ms = new MemoryStream(mess);
+            CryptoStream cs = new CryptoStream(ms, mySymmetricAlg.CreateDecryptor(), CryptoStreamMode.Read);
+            cs.Read(plaintext, 0, mess.Length); cs.Close();
+            return plaintext;
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Generate(comboBox1.Text); 
+            textBox1.Text =myConverter.ByteArrayToHexString(mySymmetricAlg.Key); 
+            textBox2.Text =myConverter.ByteArrayToHexString(mySymmetricAlg.IV);
+
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] ciphertext = Encrypt(myConverter.StringToByteArray(textBox3.Text), myConverter.HexStringToByteArray(textBox1.Text), myConverter.HexStringToByteArray(textBox2.Text)); 
+            textBox5.Text = myConverter.ByteArrayToString(ciphertext); 
+            textBox6.Text =myConverter.ByteArrayToHexString(ciphertext); 
+            textBox3.Text =myConverter.ByteArrayToHexString(myConverter.StringToByteArray(textBox3.Text));
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            byte[] plaintext = Decrypt(myConverter.HexStringToByteArray(textBox6.Text),myConverter.HexStringToByteArray(textBox1.Text), myConverter.HexStringToByteArray(textBox2.Text));
+            textBox3.Text = myConverter.ByteArrayToString(plaintext);
+            textBox4.Text = myConverter.ByteArrayToHexString(plaintext);
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mySymmetricAlg.GenerateIV(); // generates a fresh IV
+            mySymmetricAlg.GenerateKey(); // generates a fresh Key
+
+            MemoryStream ms =new MemoryStream(); 
+            CryptoStream cs= new CryptoStream(ms,mySymmetricAlg.CreateEncryptor(), CryptoStreamMode.Write);
+            byte[] mes_block = new byte[8];
+            long start_time = DateTime.Now.Ticks; int count = 10000000;
+            for (int i = 0; i < count; i++)
+            {
+                cs.Write(mes_block, 0, mes_block.Length);
+            }
+            cs.Close();
+            double operation_time = (DateTime.Now.Ticks - start_time); 
+            operation_time = operation_time / (10 * count);
+            
+            label9.Text = "Time for encryption of a message block: " + operation_time.ToString() + "  us" ;
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            mySymmetricAlg.GenerateIV(); // generates a fresh IV
+            mySymmetricAlg.GenerateKey(); // generates a fresh Key
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, mySymmetricAlg.CreateDecryptor(), CryptoStreamMode.Read);
+            byte[] mes_block = new byte[8];
+            long start_time = DateTime.Now.Ticks; int count = 10000000;
+            for (int i = 0; i < count; i++)
+            {
+                cs.Read(mes_block, 0, mes_block.Length);
+            }
+            cs.Close();
+            double operation_time = (DateTime.Now.Ticks - start_time);
+            operation_time = operation_time / (10 * count);
+
+            label10.Text = "Time for decryption of a message block: " + operation_time.ToString() + "  us";
+
+        }
+    }
+}
+
